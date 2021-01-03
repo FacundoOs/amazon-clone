1f633de09d57ed1f1f03bb34e2ec4c8d264da0d9
FacundoOs <osores.facundo@gmail.com>
2021-01-03 22:27:10 +0100

    Create CheckoutProduct component and use React Context API to share data between components

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,8 +1,11 @@
 import React from "react";
 import "./Checkout.css";
 import Subtotal from "./Subtotal";
+import { useStateValue } from "../modules/StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 
 function Checkout() {
+  const [{ basket }, dispatch] = useStateValue();
   return (
     <div className="checkout">
       <div className="checkout__left">
@@ -13,12 +16,29 @@ function Checkout() {
         />
         <div>
           <h2 className="checkout__title">Your shopping Basket</h2>
+
+          {/* <CheckoutProduct
+              id="124123451"
+              title="rtwertwe"
+              image="https://images-na.ssl-images-amazon.com/images/I/51Zymoq7UnL._SX325_BO1,204,203,200_.jpg"
+              price={398.54}
+              rating={5}
+            /> */}
+
+          {basket.map((item) => (
+            <CheckoutProduct
+              id={item.id}
+              title={item.title}
+              image={item.image}
+              price={item.price}
+              rating={item.rating}
+            />
+          ))}
         </div>
       </div>
 
       <div className="checkout__right">
         <Subtotal />
-
       </div>
     </div>
   );
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,25 @@
 import React from "react";
 import "./Product.css";
+import {useStateValue} from '../modules/StateProvider'
 
-function Product({ title, image, price, rating }) {
+function Product({id, title, image, price, rating }) {
+  const [state, dispatch] = useStateValue()
+
+  console.log("this",state)
+
+  const addToBasket = () =>{
+    dispatch({
+      type: 'ADD_TO_BASKET',
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price, price,
+        rating: rating
+      }
+    })
+
+  }
   return (
     <div className="product">
       <div className="product__info">
@@ -19,7 +37,7 @@ function Product({ title, image, price, rating }) {
         </div>
       </div>
       <img className="" src={image} alt=""></img>
-      <button>Add to Basket</button>
+      <button onClick={addToBasket}>Add to Basket</button>
     </div>
   );
 }
