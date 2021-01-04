10011158cdda47829bc3bba78ee828f77d29b151
FacundoOs <osores.facundo@gmail.com>
2021-01-04 11:38:08 +0100

    Config payment with Stripe. Add axios. Add cloud functions from firebase

--- src/App.js
+++ src/App.js
@@ -1,34 +1,39 @@
-import React, {useEffect} from 'react'
+import React, { useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Home from "./components/Home";
-import Checkout from './components/Checkout'
-import Payment from './components/Payment'
+import Checkout from "./components/Checkout";
+import Payment from "./components/Payment";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Login from "./components/Login";
-import { auth } from './modules/firebase';
-import {useStateValue} from './modules/StateProvider'
+import { auth } from "./modules/firebase";
+import { useStateValue } from "./modules/StateProvider";
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
+const promise = loadStripe(
+  "pk_test_51HdYrMDq51jzAKNH4YJ57mqONMDOMjEbIiQxTz1Tm52eSsWwXZZTTVKKvHmjNljMflZ8nmeJdz4SyBzK6IuwBt0I00XuNFcgTi"
+);
 
 function App() {
-  const [{}, dispath] = useStateValue()
+  const [{}, dispath] = useStateValue();
 
   useEffect(() => {
-  auth.onAuthStateChanged(authUser => {
-    console.log('THE USER IS >>>', authUser)
-    if(authUser) {
-      dispath({
-        type: 'SET_USER',
-        user: authUser
-      })
-    } else {
-      dispath({
-        type: 'SET_USER',
-        user: null
-      })
-
-    }
-  })
-  }, [])
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>>", authUser);
+      if (authUser) {
+        dispath({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        dispath({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
 
   return (
     <Router>
@@ -42,7 +47,9 @@ function App() {
             <Checkout />
           </Route>
           <Route path="/payment">
-            <Payment />
+            <Elements stripe={promise}>
+              <Payment />
+            </Elements>
           </Route>
           <Route path="/">
             <Home />
